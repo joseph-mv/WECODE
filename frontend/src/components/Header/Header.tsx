ab8a25986e31eb1224b9ccf9126ed2abe2ccb7df
josephmv43 <josephmv43@gamil.com>
2024-12-30 16:33:07 +0530

    feat:add about us section

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+  return (
+    <header className=" flex p-3 h-20  justify-between items-center ">
+      <div className="rounded-full outline outline-mainText outline-1">
+        <img
+          style={{ width: "50px", height: "auto" }}
+          src="images/logo.png"
+          alt="logo"
+        />
+      </div>
+      </header>
+  )
+}
+
+export default Header
