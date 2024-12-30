386af6d925b6cb31a0a8db08736ff1ed696cad69
josephmv43 <josephmv43@gamil.com>
2024-12-30 12:36:04 +0530

    feat: add Hero section of home page

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial-black': ['"Arial Black"', 'Arial', 'sans-serif'],
+        'trebuchet': ['"Trebuchet MS"', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
