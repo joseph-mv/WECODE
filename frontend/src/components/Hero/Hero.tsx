a16e10b7af10d4199f5be31fae442c5e884691d8
josephmv43 <josephmv43@gamil.com>
2025-01-10 17:01:24 +0530

    style:change color and orientations of icons in Hero section

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import {
   faDiscord,
-  faInstagram,
+  faSquareInstagram,
   faTelegram,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
@@ -9,43 +9,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Hero = () => {
   return (
     <section className="relative  bg-hero bg-blend-overlay antialiased flex flex-col justify-center pl-[calc(2*10%)] pr-[10%]">
-       <div className="absolute sm:left-8 left-2 space-y-4 mt-4 sm:mt-0 flex flex-col  ">
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            className="text-orange-500   text-center animate-zoomInOut  hover:text-orange-600 hover:animate-none hover:scale-125 text-2xl"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a
-            href=" https://www.youtube.com/@wecodemalayalam
-        "
-            target="_blank"
-            className="text-orange-500   animate-zoomInOut text-center hover:text-orange-600 hover:animate-none hover:scale-125 text-2xl"
-          >
-            <FontAwesomeIcon icon={faYoutube} />
-          </a>
-
-          <a
-            href="  https://discord.com/servers/wecode-1132674812200882246
-        "
-            target="_blank"
-            className="text-orange-500  animate-zoomInOut  text-center hover:text-orange-600 hover:animate-none hover:scale-125 text-2xl"
-          >
-            <FontAwesomeIcon icon={faDiscord} />
-          </a>
-
-          <a
-            href="  https://t.me/Wecode1
-        "
-            target="_blank"
-            className="text-orange-500  animate-zoomInOut  text-center hover:text-orange-600 hover:animate-none hover:scale-125 text-2xl"
-          >
-            <FontAwesomeIcon icon={faTelegram} />
-          </a>
-        </div>
-      <div className="relative">
-       
+     {/* Social Links */}
+     
+      <div className="relative ">
         <h1 className=" md:text-6xl font-arial-black font-bold  mb-8">
           {" "}
           Build Your Future with <span className="text-blue-700">WeCode</span>
@@ -55,10 +21,47 @@ export const Hero = () => {
           learners. Get guidance, collaborate on projects, and shape your career
           in tech.
         </h3>
+
+        
         {/* <button  className=" bg-primary font-semibold w-max left- text-black gap-3 p-4   rounded-md bg-violet-400 transition-transform transform hover:scale-105 flex items-center sm:ml-[calc(2*30%)] ml-[20%]">
     <FontAwesomeIcon icon={faUsers} className="animate-zoomInOut "/>
       Join Our Community
     </button> */}
+      </div>
+      <div className="absolute bottom-10 left-[50%] -translate-x-[50%] mx-auto   space-x-[10vw] mt-4  flex   ">
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          className="text-gray-900   text-center animate-zoomInOut  hover:text-gray-800 hover:animate-none hover:scale-125 text-2xl"
+        >
+          <FontAwesomeIcon icon={faSquareInstagram} />
+        </a>
+        <a
+          href=" https://www.youtube.com/@wecodemalayalam
+        "
+          target="_blank"
+          className="text-gray-900   animate-zoomInOut text-center hover:text-gray-800 hover:animate-none hover:scale-125 text-2xl"
+        >
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+
+        <a
+          href="  https://discord.com/servers/wecode-1132674812200882246
+        "
+          target="_blank"
+          className="text-gray-900  animate-zoomInOut  text-center hover:text-gray-800 hover:animate-none hover:scale-125 text-2xl"
+        >
+          <FontAwesomeIcon icon={faDiscord} />
+        </a>
+
+        <a
+          href="  https://t.me/Wecode1
+        "
+          target="_blank"
+          className="text-gray-900  animate-zoomInOut  text-center hover:text-gray-800 hover:animate-none hover:scale-125 text-2xl"
+        >
+          <FontAwesomeIcon icon={faTelegram} />
+        </a>
       </div>
     </section>
   );
